2ba263d7bda36092d68a78975e4cc426b5984464
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2022-04-27 19:54:59 +0200

    Changed testing segment to use acceleration

--- src/main/java/net/bandithemepark/bandicore/park/attractions/tracks/segments/types/TestSegment.kt
+++ src/main/java/net/bandithemepark/bandicore/park/attractions/tracks/segments/types/TestSegment.kt
@@ -2,21 +2,25 @@ package net.bandithemepark.bandicore.park.attractions.tracks.segments.types
 
 import net.bandithemepark.bandicore.park.attractions.tracks.segments.SegmentType
 import net.bandithemepark.bandicore.park.attractions.tracks.vehicles.TrackVehicle
+import net.bandithemepark.bandicore.park.attractions.tracks.vehicles.actions.types.AccelerateAction
 import net.bandithemepark.bandicore.util.TrackUtil
 
 class TestSegment: SegmentType("test", true, "DISPATCH_SPEED_KMH") {
     var pastMiddle = false
+    var dispatched = false
 
     override fun onVehicleEnter(vehicle: TrackVehicle) {
         vehicle.physicsType = TrackVehicle.PhysicsType.NONE
 
+        dispatched = false
         pastMiddle = false
     }
 
     override fun onVehicleUpdate(vehicle: TrackVehicle) {
         if(pastMiddle) {
-            if(isNextBlockClear()) {
-                vehicle.speedKMH = metadata[0].toDouble()
+            if(isNextBlockClear() && !dispatched) {
+                dispatched = true
+                AccelerateAction.to(vehicle, metadata[0].toDouble(), 20)
             }
         } else {
             if(TrackUtil.isPastMiddle(parent, vehicle)) {
