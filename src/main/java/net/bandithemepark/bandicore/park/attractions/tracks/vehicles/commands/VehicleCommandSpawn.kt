e815247d481d3d1d250acf2280975317cade12b9
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2022-05-20 09:26:58 +0200

    Vehicle Spawn Command now runs async

--- src/main/java/net/bandithemepark/bandicore/park/attractions/tracks/vehicles/commands/VehicleCommandSpawn.kt
+++ src/main/java/net/bandithemepark/bandicore/park/attractions/tracks/vehicles/commands/VehicleCommandSpawn.kt
@@ -4,7 +4,9 @@ import net.bandithemepark.bandicore.BandiCore
 import net.bandithemepark.bandicore.park.attractions.tracks.TrackPosition
 import net.bandithemepark.bandicore.server.translations.LanguageUtil.sendTranslatedMessage
 import net.bandithemepark.bandicore.util.chat.BandiColors
+import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
+import org.bukkit.scheduler.BukkitRunnable
 
 class VehicleCommandSpawn: TrackVehicleCommand("spawn", "<vehicleID> <trackID>") {
     override fun onUse(sender: CommandSender, args: MutableList<String>) {
@@ -12,8 +14,12 @@ class VehicleCommandSpawn: TrackVehicleCommand("spawn", "<vehicleID> <trackID>")
             val track = BandiCore.instance.trackManager.loadedTracks.find { it.id == args[1] }
 
             if(track != null) {
-                BandiCore.instance.trackManager.vehicleManager.loadTrain(args[0], track, TrackPosition(track.nodes[0], 0), 10.0)
-                sender.sendTranslatedMessage("track-vehicle-command-spawned", BandiColors.YELLOW.toString())
+                object: BukkitRunnable() {
+                    override fun run() {
+                        BandiCore.instance.trackManager.vehicleManager.loadTrain(args[0], track, TrackPosition(track.nodes[0], 0), 10.0)
+                        sender.sendTranslatedMessage("track-vehicle-command-spawned", BandiColors.YELLOW.toString())
+                    }
+                }.runTaskAsynchronously(BandiCore.instance)
             } else {
                 sender.sendTranslatedMessage("track-not-loaded", BandiColors.RED.toString())
             }
