6e3453a17fe491d8783e2cc16f56cca7191cc87f
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2022-05-26 14:21:42 +0200

    Added log flume lifthill

---
+++ src/main/java/net/bandithemepark/bandicore/bandithemepark/adventure/logflume/segments/LogflumeLiftSegment.kt
@@ -0,0 +1,44 @@
+package net.bandithemepark.bandicore.bandithemepark.adventure.logflume.segments
+
+import net.bandithemepark.bandicore.park.attractions.tracks.segments.SegmentType
+import net.bandithemepark.bandicore.park.attractions.tracks.vehicles.TrackVehicle
+
+class LogflumeLiftSegment: SegmentType("logflumelift", true, "speed") {
+    val slowDown = hashMapOf<TrackVehicle, Boolean>()
+
+    override fun onVehicleEnter(vehicle: TrackVehicle) {
+        // Whether the boat should be slowed down or sped up.
+        slowDown[vehicle] = vehicle.speedKMH > metadata[0].toDouble()
+
+        // If it doesn't need to slow down (so speed up), then set the physics type
+        if(!slowDown[vehicle]!!) {
+            vehicle.physicsType = TrackVehicle.PhysicsType.DOWN
+        }
+    }
+
+    override fun onVehicleUpdate(vehicle: TrackVehicle) {
+        if(slowDown.containsKey(vehicle)) {
+            val shouldSlowDown = slowDown[vehicle]!!
+
+            if(shouldSlowDown) {
+                if(vehicle.speedKMH < metadata[0].toDouble()) {
+                    vehicle.speedKMH = metadata[0].toDouble()
+                    vehicle.physicsType = TrackVehicle.PhysicsType.DOWN
+                    slowDown.remove(vehicle)
+                }
+            } else {
+                vehicle.speedKMH += metadata[0].toDouble()/30.0
+
+                if(vehicle.speedKMH > metadata[0].toDouble()) {
+                    vehicle.speedKMH = metadata[0].toDouble()
+                    slowDown.remove(vehicle)
+                }
+            }
+        }
+    }
+
+    override fun onVehicleLeave(vehicle: TrackVehicle) {
+        slowDown.remove(vehicle)
+        vehicle.physicsType = TrackVehicle.PhysicsType.ALL
+    }
+}
