eba4f1636c71a3eb7a36d2b9cdb45219556ef11e
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2022-06-04 11:46:11 +0200

    Update Logflumeattraction to make use of new system

--- src/main/java/net/bandithemepark/bandicore/bandithemepark/adventure/logflume/LogFlumeAttraction.kt
+++ src/main/java/net/bandithemepark/bandicore/bandithemepark/adventure/logflume/LogFlumeAttraction.kt
@@ -1,4 +1,16 @@
 package net.bandithemepark.bandicore.bandithemepark.adventure.logflume
 
-class LogFlumeAttraction {
+import net.bandithemepark.bandicore.park.attractions.Attraction
+import net.bandithemepark.bandicore.park.attractions.AttractionAppearance
+import org.bukkit.Material
+import org.bukkit.entity.Player
+
+class LogFlumeAttraction: Attraction("logflume", AttractionAppearance("Logflume", listOf("Blame Youri for not having", "added a description yet"), Material.DIAMOND_HOE, 1)) {
+    override fun getPlayerPassengers(): List<Player> {
+        return emptyList()
+    }
+
+    override fun onAttractionStart() {
+
+    }
 }
