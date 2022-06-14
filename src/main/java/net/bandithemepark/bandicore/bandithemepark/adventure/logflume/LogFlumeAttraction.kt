a466b8408e41f4d5db0f84c40e309f3333e73f16
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2022-06-14 18:47:48 +0200

    Updated description and exiting location of logflume

--- src/main/java/net/bandithemepark/bandicore/bandithemepark/adventure/logflume/LogFlumeAttraction.kt
+++ src/main/java/net/bandithemepark/bandicore/bandithemepark/adventure/logflume/LogFlumeAttraction.kt
@@ -2,10 +2,15 @@ package net.bandithemepark.bandicore.bandithemepark.adventure.logflume
 
 import net.bandithemepark.bandicore.park.attractions.Attraction
 import net.bandithemepark.bandicore.park.attractions.AttractionAppearance
+import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.entity.Player
 
-class LogFlumeAttraction: Attraction("logflume", AttractionAppearance("Logflume", listOf("Join Bandiana Jones on his adventure", "and uncover the secrets of a newly", "discovered temple. Be prepared for", "things to get out of hand!"), Material.DIAMOND_HOE, 1)) {
+class LogFlumeAttraction: Attraction("logflume",
+    AttractionAppearance("Logflume", listOf("Join Bandiana Jones on his adventure", "and uncover the secrets of a newly", "discovered temple. Be prepared for", "things to get out of hand!"), Material.DIAMOND_HOE, 1),
+    Location(Bukkit.getWorld("world"), 63.5, 1.0, -171.5, -45.0F, 0.0F)
+    ) {
     override fun getPlayerPassengers(): List<Player> {
         return emptyList()
     }
