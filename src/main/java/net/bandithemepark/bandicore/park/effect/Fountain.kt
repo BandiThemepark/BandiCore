cb4507e6f401c489607495c52c3a2e0555717171
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2022-07-05 20:26:21 +0200

    Added fountain system

---
+++ src/main/java/net/bandithemepark/bandicore/park/effect/Fountain.kt
@@ -0,0 +1,27 @@
+package net.bandithemepark.bandicore.park.effect
+
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.Particle
+import org.bukkit.inventory.ItemStack
+import org.bukkit.util.Vector
+import kotlin.random.Random
+
+class Fountain(val location: Location, var direction: Vector): AmbientEffect() {
+    var active = true
+
+    override fun onTick() {
+        if(!active) return
+
+        location.world!!.spawnParticle(Particle.ITEM_CRACK, location, 0, direction.x, direction.y, direction.z, getParticleBlock())
+    }
+
+    fun getParticleBlock(): ItemStack {
+        return when(Random.nextInt(2)) {
+            0 -> ItemStack(Material.CYAN_STAINED_GLASS)
+            1 -> ItemStack(Material.BLUE_STAINED_GLASS)
+            else -> ItemStack(Material.WATER)
+        }
+    }
+}
