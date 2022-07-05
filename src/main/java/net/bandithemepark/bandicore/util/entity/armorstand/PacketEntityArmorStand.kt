270c897bcbaee39f6b83c474b43b40228805d435
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2022-07-05 20:25:46 +0200

    ArmorStands can now glow per person

--- src/main/java/net/bandithemepark/bandicore/util/entity/armorstand/PacketEntityArmorStand.kt
+++ src/main/java/net/bandithemepark/bandicore/util/entity/armorstand/PacketEntityArmorStand.kt
@@ -3,10 +3,15 @@ package net.bandithemepark.bandicore.util.entity.armorstand
 import net.bandithemepark.bandicore.util.entity.PacketEntity
 import net.bandithemepark.bandicore.util.math.MathUtil
 import net.bandithemepark.bandicore.util.math.Quaternion
+import net.kyori.adventure.text.Component
 import net.minecraft.core.Rotations
+import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.decoration.ArmorStand
+import org.bukkit.Bukkit
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer
+import org.bukkit.entity.Player
 import org.bukkit.util.Vector
 
 open class PacketEntityArmorStand: PacketEntity() {
@@ -64,5 +69,37 @@ open class PacketEntityArmorStand: PacketEntity() {
         val armorStandPose = MathUtil.getArmorStandPose(rotation)
         setHeadPose(Math.toDegrees(armorStandPose.x), Math.toDegrees(armorStandPose.y), Math.toDegrees(armorStandPose.z))
         moveEntity(position.x, position.y, position.z)
+    }
+
+    override fun updateMetadata() {
+        for(player in getPlayersVisibleFor()) {
+            if(glowingFor.contains(player)) {
+                val before = (this as PacketEntity).handle!!.hasGlowingTag()
+                (this as PacketEntity).handle!!.setGlowingTag(true)
+                val packet = ClientboundSetEntityDataPacket(handle!!.id, handle!!.entityData, true)
+                (player as CraftPlayer).handle.connection.send(packet)
+                (this as PacketEntity).handle!!.setGlowingTag(before)
+            } else {
+                val packet = ClientboundSetEntityDataPacket(handle!!.id, handle!!.entityData, true)
+                (player as CraftPlayer).handle.connection.send(packet)
+            }
+        }
+    }
+
+    val glowingFor = mutableListOf<Player>()
+    fun startGlowFor(player: Player) {
+        glowingFor.add(player)
+        val before = (this as PacketEntity).handle!!.hasGlowingTag()
+        (this as PacketEntity).handle!!.setGlowingTag(true)
+        (this as PacketEntity).updateMetadataFor(player)
+        (this as PacketEntity).handle!!.setGlowingTag(before)
+    }
+
+    fun endGlowFor(player: Player) {
+        glowingFor.remove(player)
+        val before = (this as PacketEntity).handle!!.hasGlowingTag()
+        (this as PacketEntity).handle!!.setGlowingTag(false)
+        (this as PacketEntity).updateMetadataFor(player)
+        (this as PacketEntity).handle!!.setGlowingTag(before)
     }
 }
