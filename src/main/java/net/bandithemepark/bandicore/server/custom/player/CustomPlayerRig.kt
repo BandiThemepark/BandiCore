a534345a345765aff75d2305c79dff09b5488204
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2023-07-24 19:52:44 +0200

    Fixed custom players

--- src/main/java/net/bandithemepark/bandicore/server/custom/player/CustomPlayerRig.kt
+++ src/main/java/net/bandithemepark/bandicore/server/custom/player/CustomPlayerRig.kt
@@ -1,9 +1,13 @@
 package net.bandithemepark.bandicore.server.custom.player
 
 import net.bandithemepark.bandicore.server.animatronics.Animatronic
+import net.bandithemepark.bandicore.server.animatronics.AnimatronicNode
 import net.bandithemepark.bandicore.util.ItemFactory
+import net.bandithemepark.bandicore.util.entity.armorstand.PacketEntityArmorStand
 import net.bandithemepark.bandicore.util.entity.itemdisplay.PacketItemDisplay
 import net.bandithemepark.bandicore.util.math.Quaternion
+import net.minecraft.world.entity.decoration.ArmorStand
+import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.entity.ItemDisplay
@@ -11,12 +15,34 @@ import org.bukkit.util.Vector
 
 class CustomPlayerRig(val skin: CustomPlayerSkin) {
     lateinit var animatronic: Animatronic
+    lateinit var parentArmorStand: PacketEntityArmorStand
 
     fun spawn(spawnLocation: Location) {
+        // Remove rotation here, because rotation is added later using transform
         spawnLocation.pitch = 0.0f
         spawnLocation.yaw = 0.0f
 
+        // Spawn parent armorstand for smoothness
+        parentArmorStand = PacketEntityArmorStand()
+        parentArmorStand.spawn(spawnLocation)
+        parentArmorStand.handle!!.isInvisible = true
+        parentArmorStand.handle!!.isNoGravity = true
+        (parentArmorStand.handle!! as ArmorStand).isMarker = true
+        parentArmorStand.updateMetadata()
+
+        // Spawn animatronic
         animatronic = Animatronic("player_rig")
+
+        // Update spawn order to make sure textures are correct
+        val newNodes = mutableListOf<AnimatronicNode>()
+        newNodes.add(animatronic.nodes.find { it.name == "body" }!!)
+        newNodes.add(animatronic.nodes.find { it.name == "head" }!!)
+        newNodes.add(animatronic.nodes.find { it.name == "left_arm" }!!)
+        newNodes.add(animatronic.nodes.find { it.name == "right_arm" }!!)
+        newNodes.add(animatronic.nodes.find { it.name == "left_leg" }!!)
+        newNodes.add(animatronic.nodes.find { it.name == "right_leg" }!!)
+        animatronic.nodes = newNodes
+
         animatronic.spawn(spawnLocation, Quaternion.fromYawPitchRoll(0.0, 0.0, 0.0))
 
         updateDisplayEntities()
@@ -32,15 +58,16 @@ class CustomPlayerRig(val skin: CustomPlayerSkin) {
     }
 
     fun moveTo(position: Vector, rotation: Quaternion) {
-        animatronic.setBasePosition(position)
+        parentArmorStand.moveEntity(position.x, position.y, position.z)
         animatronic.baseRotation = rotation
     }
 
     private fun updateDisplayEntities() {
         animatronic.displayEntities.values.forEach {
-            it.setInterpolationDuration(10)
             it.setItemDisplayTransform(ItemDisplay.ItemDisplayTransform.THIRDPERSON_RIGHTHAND)
+            parentArmorStand.addPassenger(it.handle!!.id)
         }
+        parentArmorStand.updatePassengers()
 
         setItem("body", 7)
         setItem("head", 8)
