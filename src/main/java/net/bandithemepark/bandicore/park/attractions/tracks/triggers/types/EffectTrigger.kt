f326a7d88edce0a94c0134a60124a91c8692d2b2
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2023-07-26 18:31:57 +0200

    Fixed effect being able to play twice in EffectTrigger

--- src/main/java/net/bandithemepark/bandicore/park/attractions/tracks/triggers/types/EffectTrigger.kt
+++ src/main/java/net/bandithemepark/bandicore/park/attractions/tracks/triggers/types/EffectTrigger.kt
@@ -1,11 +1,15 @@
 package net.bandithemepark.bandicore.park.attractions.tracks.triggers.types
 
+import net.bandithemepark.bandicore.BandiCore
 import net.bandithemepark.bandicore.park.attractions.tracks.triggers.TrackTriggerType
 import net.bandithemepark.bandicore.park.attractions.tracks.vehicles.TrackVehicle
 import net.bandithemepark.bandicore.server.effects.Effect
+import org.bukkit.Bukkit
 
 class EffectTrigger: TrackTriggerType("effect", "EFFECT_NAME") {
     override fun onActivation(vehicle: TrackVehicle) {
+        if(BandiCore.instance.effectManager.playingEffects.any { it.fileName == metadata[0] }) return
+
         val effect = Effect(metadata[0])
         effect.play()
     }
