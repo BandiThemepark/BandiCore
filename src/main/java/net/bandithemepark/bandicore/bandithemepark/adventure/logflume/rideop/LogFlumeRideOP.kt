33406fa6f41c355adb236669c657a65474f2e6a9
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2023-07-29 21:49:20 +0200

    Added cameras to logflume

--- src/main/java/net/bandithemepark/bandicore/bandithemepark/adventure/logflume/rideop/LogFlumeRideOP.kt
+++ src/main/java/net/bandithemepark/bandicore/bandithemepark/adventure/logflume/rideop/LogFlumeRideOP.kt
@@ -8,6 +8,7 @@ import net.bandithemepark.bandicore.bandithemepark.adventure.logflume.transferon
 import net.bandithemepark.bandicore.bandithemepark.adventure.logflume.transferone.LogFlumeTransferSegment
 import net.bandithemepark.bandicore.park.attractions.rideop.RideOP
 import net.bandithemepark.bandicore.park.attractions.rideop.RideOPPage
+import net.bandithemepark.bandicore.park.attractions.rideop.camera.RideOPCameraButton
 import net.bandithemepark.bandicore.park.attractions.rideop.util.buttons.GatesButton
 import net.bandithemepark.bandicore.park.attractions.rideop.util.pages.RideOPCameraPage
 import net.bandithemepark.bandicore.park.attractions.rideop.util.pages.RideOPHomePage
@@ -70,7 +71,20 @@ class LogFlumeRideOP: RideOP(
                 LogFlumeEStop()
             )),
             RideOPCameraPage(listOf(
-
+                RideOPCameraButton(0, "Transfer", Location(Bukkit.getWorld("world"), 24.0, 12.0, -90.0, -135.0F, 40.0F), this),
+                RideOPCameraButton(1, "Station Queue", Location(Bukkit.getWorld("world"), 56.5, 4.0, -163.0, 180.0F, 20.0F), this),
+                RideOPCameraButton(2, "Lifthill 1", Location(Bukkit.getWorld("world"), 62.0, 13.7, -134.5, 170.0F, 40.0F), this),
+                RideOPCameraButton(3, "Drop 1", Location(Bukkit.getWorld("world"), 38.0, 10.5, -105.0, -60.0F, 40.0F), this),
+                RideOPCameraButton(4, "Drop 1 Splash", Location(Bukkit.getWorld("world"), 23.5, 5.55, -103.5, -70.0F, 40.0F), this),
+                RideOPCameraButton(5, "Switchtrack", Location(Bukkit.getWorld("world"), 20.0, 5.5, -97.0, 150.0F, 33.0F), this),
+                RideOPCameraButton(6, "Lifthill 2", Location(Bukkit.getWorld("world"), 55.0, 11.0, -87.0, 50.0F, 27.0F), this),
+                RideOPCameraButton(7, "Drop 2 Splash", Location(Bukkit.getWorld("world"), 33.5, 14.0, -89.5, -105.0F, 27.0F), this),
+                RideOPCameraButton(8, "Storage", Location(Bukkit.getWorld("world"), 16.5, 11.5, -92.5, 60.0F, 17.0F), this),
+                RideOPCameraButton(9, "Disco Entrance", Location(Bukkit.getWorld("world"), 13.5, 16.5, -114.5, -20.0F, 12.0F), this),
+                RideOPCameraButton(10, "Disco", Location(Bukkit.getWorld("world"), 14.5, 17.0, -95.5, -130.0F, 22.0F), this),
+                RideOPCameraButton(11, "Drop 3 Start", Location(Bukkit.getWorld("world"), 42.5, 3.5, -124.5, 45.0F, 18.0F), this),
+                RideOPCameraButton(12, "Drop 3 Airtime Hill", Location(Bukkit.getWorld("world"), 32.5, 3.0, -139.5, -45.0F, 27.0F), this),
+                RideOPCameraButton(13, "Station Boat Entrance", Location(Bukkit.getWorld("world"), 60.5, 2.0, -189.5, 60.0F, 10.0F), this),
             )),
             RideOPStoragePage(listOf(
 
