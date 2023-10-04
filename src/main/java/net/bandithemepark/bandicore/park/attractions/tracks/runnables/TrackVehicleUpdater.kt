0c4420b6242aae83a21bb0000728aedf8e5e9a79
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2023-10-04 09:59:37 +0200

    Small change to improve reliability of segment leave detection

--- src/main/java/net/bandithemepark/bandicore/park/attractions/tracks/runnables/TrackVehicleUpdater.kt
+++ src/main/java/net/bandithemepark/bandicore/park/attractions/tracks/runnables/TrackVehicleUpdater.kt
@@ -213,7 +213,7 @@ class TrackVehicleUpdater {
                         if(lastSegment == segmentSeparator) {
                             if(lastSegment.type != null) lastSegment.type!!.onVehicleUpdate(vehicle)
                         } else {
-                            if(lastSegment.vehicles.contains(vehicle)) lastSegment.vehicles.removeAt(lastSegment.vehicles.indexOf(vehicle))
+                            if(lastSegment.vehicles.contains(vehicle)) lastSegment.vehicles.remove(vehicle)
                             segmentSeparator.vehicles.add(vehicle)
 
                             if(lastSegment.type != null) lastSegment.type!!.onVehicleLeave(vehicle)
