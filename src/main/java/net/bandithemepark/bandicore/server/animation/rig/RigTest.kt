f0f02f452577b1342af544b72ddc7fb95a754248
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-04-16 21:48:50 +0200

    Added test for block display groups

--- src/main/java/net/bandithemepark/bandicore/server/animation/rig/RigTest.kt
+++ src/main/java/net/bandithemepark/bandicore/server/animation/rig/RigTest.kt
@@ -4,6 +4,7 @@ import net.bandithemepark.bandicore.BandiCore
 import net.bandithemepark.bandicore.server.animatronics.Animatronic
 import net.bandithemepark.bandicore.server.custom.player.CustomPlayerRig
 import net.bandithemepark.bandicore.server.custom.player.CustomPlayerSkin.Companion.getAdaptedSkin
+import net.bandithemepark.bandicore.server.effects.blockdisplaygroup.BlockDisplayGroup
 import net.bandithemepark.bandicore.util.ItemFactory
 import net.bandithemepark.bandicore.util.Util
 import net.bandithemepark.bandicore.util.chat.BandiColors
@@ -12,6 +13,7 @@ import net.bandithemepark.bandicore.util.entity.display.PacketTextDisplay
 import net.bandithemepark.bandicore.util.math.Quaternion
 import org.bukkit.Bukkit
 import org.bukkit.Color
+import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
@@ -20,6 +22,7 @@ import org.bukkit.entity.Display
 import org.bukkit.entity.ItemDisplay
 import org.bukkit.entity.Player
 import org.bukkit.entity.TextDisplay
+import org.bukkit.util.Vector
 import org.joml.Matrix4f
 
 class RigTest: CommandExecutor {
@@ -27,9 +30,25 @@ class RigTest: CommandExecutor {
         if(!command.name.equals("rigtest", true)) return false
         if(sender !is Player) return false
 
-        val customPlayer = CustomPlayerRig(sender.getAdaptedSkin())
-        customPlayer.spawn(sender.location.clone().add(0.0, 0.0, 0.0), null)
-        customPlayer.playAnimationLooped("stand")
+        val group = BlockDisplayGroup(
+            Location(sender.world,  -43.0, 8.0, -85.0),
+            Location(sender.world,  -61.0, 18.0, -103.0),
+            Vector(-51.5, 8.0, -93.5),
+            Vector(0.0, 0.0, -10.0),
+            Vector(0.0, 12.0, 0.0)
+        )
+
+        group.spawn()
+        group.update()
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(BandiCore.instance, Runnable {
+            group.rotation = group.rotation.clone().add(Vector(0.0, 2.0, 0.0))
+            group.update()
+        }, 1, 0)
+
+//        val customPlayer = CustomPlayerRig(sender.getAdaptedSkin())
+//        customPlayer.spawn(sender.location.clone().add(0.0, 0.0, 0.0), null)
+//        customPlayer.playAnimationLooped("stand")
 
 
 //        val bukkitDisplay = sender.world.spawnEntity(sender.location, EntityType.ITEM_DISPLAY) as CraftItemDisplay
