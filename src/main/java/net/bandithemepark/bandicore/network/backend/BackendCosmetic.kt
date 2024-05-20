cafd6073073066975f662b6d07b5daa4e6cefd5d
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-05-20 14:10:26 +0200

    Added methods to give and remove cosmetics

--- src/main/java/net/bandithemepark/bandicore/network/backend/BackendCosmetic.kt
+++ src/main/java/net/bandithemepark/bandicore/network/backend/BackendCosmetic.kt
@@ -1,12 +1,16 @@
 package net.bandithemepark.bandicore.network.backend
 
 import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import net.bandithemepark.bandicore.BandiCore
+import net.bandithemepark.bandicore.park.cosmetics.Cosmetic
 import net.bandithemepark.bandicore.util.Util
 import okhttp3.Call
 import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import org.bukkit.entity.Player
 import java.io.IOException
@@ -60,6 +64,66 @@ object BackendCosmetic {
                 } else {
                     BandiCore.instance.logger.severe("An attempt was made at loading owned cosmetics of player ${player.name}, but no data was found. The following message was given: ${responseJson.get("message")}")
                 }
+            }
+        })
+    }
+
+    /**
+     * Gives a cosmetic to a player.
+     * @param player The player to give the cosmetic to
+     * @param cosmetic The cosmetic to give
+     * @param callback What to do when the cosmetic is given
+     */
+    fun give(player: Player, cosmetic: Cosmetic, callback: () -> Unit) {
+        val client = BandiCore.instance.okHttpClient
+        val mediaType = "application/json".toMediaTypeOrNull()
+
+        val data = JsonObject()
+        data.addProperty("cosmeticId", cosmetic.id.toString())
+
+        val request = Request.Builder()
+            .url("https://api.bandithemepark.net/cosmetics/give/${player.uniqueId}")
+            .method("POST", data.toString().toRequestBody(mediaType))
+            .header("Authorization", BandiCore.instance.server.apiKey)
+            .build()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                callback.invoke()
+            }
+        })
+    }
+
+    /**
+     * Removes a cosmetic from a player.
+     * @param player The player to remove the cosmetic from
+     * @param cosmetic The cosmetic to remove
+     * @param callback What to do when the cosmetic is removed
+     */
+    fun remove(player: Player, cosmetic: Cosmetic, callback: () -> Unit) {
+        val client = BandiCore.instance.okHttpClient
+        val mediaType = "application/json".toMediaTypeOrNull()
+
+        val data = JsonObject()
+        data.addProperty("cosmeticId", cosmetic.id.toString())
+
+        val request = Request.Builder()
+            .url("https://api.bandithemepark.net/cosmetics/remove/${player.uniqueId}")
+            .method("POST", data.toString().toRequestBody(mediaType))
+            .header("Authorization", BandiCore.instance.server.apiKey)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                callback.invoke()
             }
         })
     }
