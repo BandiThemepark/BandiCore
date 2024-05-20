a35f4a0ccb42b863a37b6727e8a6118c4f8dbd85
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-05-20 13:22:45 +0200

    Made most metadata optional in HatCosmetic

--- src/main/java/net/bandithemepark/bandicore/park/cosmetics/types/HatCosmetic.kt
+++ src/main/java/net/bandithemepark/bandicore/park/cosmetics/types/HatCosmetic.kt
@@ -1,26 +1,28 @@
 package net.bandithemepark.bandicore.park.cosmetics.types
 
 import com.google.gson.JsonObject
+import net.bandithemepark.bandicore.BandiCore
 import net.bandithemepark.bandicore.park.cosmetics.Cosmetic
 import net.bandithemepark.bandicore.park.cosmetics.CosmeticType
 import net.bandithemepark.bandicore.server.essentials.ranks.nametag.PlayerNameTag.Companion.getNameTag
 import net.bandithemepark.bandicore.util.ItemFactory
+import org.bukkit.Bukkit
 import org.bukkit.Color
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
 class HatCosmetic: CosmeticType("hat") {
-    lateinit var material: Material
+    var material: Material = Material.DIAMOND_SWORD
     var customModelData = 0
     var colorable = false
     var heightOffset = 0.0
 
     override fun onMetadataLoad(metadata: JsonObject) {
-        material = Material.matchMaterial(metadata.get("material").asString.uppercase())!!
-        customModelData = metadata.get("customModelData").asInt
-        colorable = metadata.get("colorable").asBoolean
-        heightOffset = metadata.get("heightOffset").asDouble
+        if(metadata.has("material")) material = Material.matchMaterial(metadata.get("material").asString.uppercase())!!
+        if(metadata.has("customModelData")) customModelData = metadata.get("customModelData").asInt
+        if(metadata.has("colorable")) colorable = metadata.get("colorable").asBoolean
+        if(metadata.has("heightOffset")) heightOffset = metadata.get("heightOffset").asDouble
     }
 
     override fun isColorable(): Boolean {
@@ -28,7 +30,7 @@ class HatCosmetic: CosmeticType("hat") {
     }
 
     override fun onEquip(player: Player, color: Color?, cosmetic: Cosmetic) {
-        val itemStack = if(colorable) ItemFactory(material).setCustomModelData(customModelData).setDisplayName(cosmetic.getItemName()).setLore(cosmetic.getItemStackDescription()).setArmorColor(color).build()
+        val itemStack = if(colorable) ItemFactory(Material.LEATHER_HORSE_ARMOR).setCustomModelData(customModelData).setDisplayName(cosmetic.getItemName()).setLore(cosmetic.getItemStackDescription()).setArmorColor(color).build()
                         else ItemFactory(material).setCustomModelData(customModelData).setDisplayName(cosmetic.getItemName()).setLore(cosmetic.getItemStackDescription()).build()
 
         player.equipment.helmet = itemStack
