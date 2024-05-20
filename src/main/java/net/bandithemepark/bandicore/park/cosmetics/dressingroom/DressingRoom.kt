7d917bf62b4629fb4387b316e50f361d588dd538
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-05-20 23:34:28 +0200

    Update dressing room positions and camera

--- src/main/java/net/bandithemepark/bandicore/park/cosmetics/dressingroom/DressingRoom.kt
+++ src/main/java/net/bandithemepark/bandicore/park/cosmetics/dressingroom/DressingRoom.kt
@@ -12,13 +12,13 @@ import org.bukkit.util.Vector
 class DressingRoom {
     val world = Bukkit.getWorld("world")!!
     val playerPosition: Vector = Vector(68.5, 0.0, -150.5)
-    val playerYaw: Double = -54.5
-    val cameraPosition: Vector = Vector(70.25, 2.2, -148.2)
-    val cameraYaw: Double = 109.6
-    val cameraPitch: Double = 0.0
+    val playerYaw: Double = -45.0
+    val cameraPosition: Vector = Vector(70.5, 1.4, -148.5)
+    val cameraYaw: Double = 135.0
+    val cameraPitch: Double = -8.0
 
     fun spawnDecorations() {
-        spawnDecoration(ItemFactory(Material.DIAMOND_SHOVEL).setCustomModelData(17).build(), Location(Bukkit.getWorld("world")!!, 66.0, 1.5,-153.0, -45f, 0f))
+        spawnDecoration(ItemFactory(Material.DIAMOND_SHOVEL).setCustomModelData(17).build(), Location(Bukkit.getWorld("world")!!, 70.5, 1.5,-153.0, 0f, 0f))
     }
 
     private fun spawnDecoration(itemStack: ItemStack, location: Location) {
