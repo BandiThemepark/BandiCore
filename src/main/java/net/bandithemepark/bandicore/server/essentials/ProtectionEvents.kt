d9b4cab20e56af4bf0ebd48f5636ef1144b9b6dc
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-06-18 17:34:40 +0200

    Add server border

--- src/main/java/net/bandithemepark/bandicore/server/essentials/ProtectionEvents.kt
+++ src/main/java/net/bandithemepark/bandicore/server/essentials/ProtectionEvents.kt
@@ -1,6 +1,12 @@
 package net.bandithemepark.bandicore.server.essentials
 
+import net.bandithemepark.bandicore.server.regions.events.PlayerRegionLeaveEvent
+import net.bandithemepark.bandicore.server.translations.LanguageUtil.sendTranslatedActionBar
+import net.bandithemepark.bandicore.util.chat.BandiColors
+import org.bukkit.Bukkit
 import org.bukkit.GameMode
+import org.bukkit.Material
+import org.bukkit.Particle
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -46,5 +52,25 @@ class ProtectionEvents: Listener {
     @EventHandler
     fun onFoodLevelChange(event: FoodLevelChangeEvent) {
         event.entity.foodLevel = 20
+    }
+
+    @EventHandler
+    fun onRegionExit(event: PlayerRegionLeaveEvent) {
+        if(event.fromRegion.name != "border") return
+        if(event.toRegion !=  null) return
+
+        if(event.player.hasPermission("bandithemepark.crew")) {
+            event.player.sendTranslatedActionBar("border-crew", BandiColors.YELLOW.toString())
+            return
+        }
+
+        event.player.spawnParticle(Particle.BLOCK_MARKER, event.toLocation.blockX + 0.5, event.toLocation.blockY + 1.5, event.toLocation.blockZ + 0.5, 1, Bukkit.createBlockData(Material.BARRIER))
+        event.player.sendTranslatedActionBar("border-no-permission", BandiColors.RED.toString())
+
+        val delta = event.toLocation.toVector().subtract(event.fromLocation.toVector()).multiply(-1.5)
+        val toLocation = event.fromLocation.toVector().add(delta).toLocation(event.fromLocation.world)
+        toLocation.yaw = event.player.location.yaw
+        toLocation.pitch = event.player.location.pitch
+        event.player.teleport(toLocation)
     }
 }
