eefa7a0e66746041e59404009a5b8dfc51b9f4cc
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-11-01 19:15:40 +0100

    Implement cosmetic achievement reward

--- src/main/java/net/bandithemepark/bandicore/server/achievements/rewards/AchievementRewardItem.kt
+++ src/main/java/net/bandithemepark/bandicore/server/achievements/rewards/AchievementRewardItem.kt
@@ -1,15 +1,31 @@
 package net.bandithemepark.bandicore.server.achievements.rewards
 
+import net.bandithemepark.bandicore.BandiCore
+import net.bandithemepark.bandicore.network.backend.BackendCosmetic
+import net.bandithemepark.bandicore.park.cosmetics.Cosmetic
+import net.bandithemepark.bandicore.park.cosmetics.CosmeticManager.Companion.getOwnedCosmetics
+import net.bandithemepark.bandicore.park.cosmetics.OwnedCosmetic
 import net.bandithemepark.bandicore.server.achievements.AchievementRewardType
 import org.bukkit.entity.Player
 
 class AchievementRewardItem: AchievementRewardType("ITEM") {
     override fun onReward(player: Player, rewardValue: String) {
-        // TODO Add later when backpack has been implemented
+        val cosmetic = getCosmetic(rewardValue) ?: return
+        BackendCosmetic.give(player, cosmetic) {
+            player.getOwnedCosmetics()!!.ownedCosmetics.add(OwnedCosmetic(cosmetic, false, 1, null))
+        }
     }
 
     override fun getRewardText(rewardValue: String): String {
-        // TODO Add later when backpack has been implemented
-        return "Item $rewardValue"
+        val cosmetic = getCosmetic(rewardValue)
+        return if(cosmetic != null) {
+            "Cosmetic ${cosmetic.displayName}"
+        } else {
+            "Unknown cosmetic"
+        }
+    }
+
+    private fun getCosmetic(rewardValue: String): Cosmetic? {
+        return BandiCore.instance.cosmeticManager.cosmetics.find { it.name == rewardValue }
     }
 }
