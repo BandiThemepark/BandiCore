f9172e235d122d5c2be1a48a377b3ab3e7185b11
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-11-04 15:14:12 +0100

    Fix attractions in attraction menu having attributes

--- src/main/java/net/bandithemepark/bandicore/park/attractions/AttractionAppearance.kt
+++ src/main/java/net/bandithemepark/bandicore/park/attractions/AttractionAppearance.kt
@@ -1,6 +1,7 @@
 package net.bandithemepark.bandicore.park.attractions
 
 import net.bandithemepark.bandicore.BandiCore
+import net.bandithemepark.bandicore.util.ItemFactory
 import net.bandithemepark.bandicore.util.Util
 import net.bandithemepark.bandicore.util.chat.BandiColors
 import net.kyori.adventure.text.Component
@@ -13,12 +14,7 @@ import org.bukkit.persistence.PersistentDataType
 
 class AttractionAppearance(val displayName: String, val description: List<String>, val material: Material, val customModelData: Int) {
     fun getItemStack(player: Player, attraction: Attraction): ItemStack? {
-        val item = ItemStack(material)
-        val meta = item.itemMeta
-
-        meta.displayName(Util.color("<!i><${BandiColors.YELLOW}>$displayName"))
-        meta.setCustomModelData(customModelData)
-        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES)
+        val itemFactory = ItemFactory(material).setDisplayName(Util.color("<!i><${BandiColors.YELLOW}>$displayName")).setCustomModelData(customModelData)
 
         val lore = mutableListOf<Component>()
         lore.add(Util.color("<!i><${attraction.mode.textColor}>${attraction.mode.text}"))
@@ -26,16 +22,13 @@ class AttractionAppearance(val displayName: String, val description: List<String
         lore.add(Component.text(" "))
         lore.add(Util.color("<!i><${BandiColors.YELLOW}>You have ridden this attraction ${BandiCore.instance.server.ridecounterManager.getRidecountOnOf(player, attraction.id)} times")) // TODO translatable
         lore.add(Util.color("<!i><${BandiColors.YELLOW}>Click here to warp to this attraction")) // TODO Translatable
-        meta.lore(lore)
+        itemFactory.setLore(lore)
 
         if(attraction.mode.glow) {
-            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS)
-            meta.addEnchant(org.bukkit.enchantments.Enchantment.SILK_TOUCH, 1, true)
+            itemFactory.addGlow()
         }
 
-        meta!!.persistentDataContainer.set(NamespacedKey(BandiCore.instance, "attraction"), PersistentDataType.STRING, attraction.id)
-
-        item.itemMeta = meta
-        return item
+        itemFactory.setKeyInPersistentStorage("attraction", attraction.id)
+        return itemFactory.build()
     }
 }
