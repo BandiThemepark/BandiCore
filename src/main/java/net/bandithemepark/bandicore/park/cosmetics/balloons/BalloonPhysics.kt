d08d3f297e32f24b4dd78b52cfd5e4a1df6e9dfa
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-11-18 16:07:04 +0100

    Wind tilting physics

--- src/main/java/net/bandithemepark/bandicore/park/cosmetics/balloons/BalloonPhysics.kt
+++ src/main/java/net/bandithemepark/bandicore/park/cosmetics/balloons/BalloonPhysics.kt
@@ -1,8 +1,12 @@
 package net.bandithemepark.bandicore.park.cosmetics.balloons
 
 import net.bandithemepark.bandicore.util.math.MathUtil
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
 import org.bukkit.util.Vector
 import kotlin.math.atan2
+import kotlin.math.min
+import kotlin.math.sin
 
 class BalloonPhysics(attachmentPoint: Vector, val onDespawn: () -> Unit) {
     var attachmentPoint: Vector? = attachmentPoint
@@ -62,7 +66,34 @@ class BalloonPhysics(attachmentPoint: Vector, val onDespawn: () -> Unit) {
         targetPitch = MathUtil.lerp(0.0, 90.0, horizontalVelocity.length() / 0.05)
         actualPitch += (targetPitch - actualPitch) * PITCH_ROTATION_SPEED
 
-        rotation = Vector(actualPitch, actualYaw+90, 0.0)
+        updateWindSimulation()
+        rotation = Vector(actualPitch, actualYaw+90, 0.0).add(windRotationOffset)
+
+    }
+
+    private var idleTime = 0
+    private var windTime = 0
+    private var windRotationOffset = Vector()
+
+    private fun updateWindSimulation() {
+        windTime++
+
+        idleTime++
+        if(idleTime > FULL_WIND_EFFECT_TIME) idleTime = FULL_WIND_EFFECT_TIME
+
+        val horizontalDistance = Vector(velocity.x, 0.0, velocity.z).length()
+        if(horizontalDistance > IDLE_RESET_DISTANCE) {
+            idleTime = 0
+        }
+
+        val pitchOffset = sin(windTime.toDouble() / WIND_TILT_TIME_PITCH * Math.PI * 2) * WIND_TILT_STRENGTH
+        val yawOffset = sin(windTime.toDouble() / WIND_TILT_TIME_YAW * Math.PI * 2) * WIND_TILT_STRENGTH
+
+        windRotationOffset = Vector(pitchOffset, yawOffset, 0.0)
+
+        // Dampen the effect when idleTime is at 0, with full effect at 20
+        val idleDampening = idleTime.toDouble() / FULL_WIND_EFFECT_TIME
+        windRotationOffset = windRotationOffset.multiply(idleDampening)
     }
 
     companion object {
@@ -74,5 +105,10 @@ class BalloonPhysics(attachmentPoint: Vector, val onDespawn: () -> Unit) {
         const val MAX_ROPE_LENGTH = 3.0
         const val YAW_ROTATION_SPEED = 0.05
         const val PITCH_ROTATION_SPEED = 0.1
+        const val IDLE_RESET_DISTANCE = 0.005
+        const val WIND_TILT_STRENGTH = 5.0
+        const val FULL_WIND_EFFECT_TIME = 20
+        const val WIND_TILT_TIME_PITCH = 73
+        const val WIND_TILT_TIME_YAW = 80
     }
 }
