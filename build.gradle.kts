8c217c1656de870b087a5ae81f7546514ce82fa0
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-12-17 19:41:24 +0100

    Add unit testing libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,12 +46,18 @@ dependencies {
     compileOnly("javax.annotation:javax.annotation-api:1.3.2")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.10")
     compileOnly("com.comphenix.protocol", "ProtocolLib", "5.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.15")
     implementation("me.m56738:SmoothCoastersAPI:1.7")
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("io.mockk:mockk:1.13.7")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 tasks {
