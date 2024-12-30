469991f025dc75348c1c1d5226095346e3fb042c
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-12-30 16:38:53 +0100

    Recreate all teams on scoreboard update

--- src/main/java/net/bandithemepark/bandicore/server/essentials/ranks/scoreboard/BandiScoreboard.kt
+++ src/main/java/net/bandithemepark/bandicore/server/essentials/ranks/scoreboard/BandiScoreboard.kt
@@ -8,78 +8,87 @@ import net.bandithemepark.bandicore.util.Util
 import net.bandithemepark.bandicore.util.chat.BandiColors
 import net.bandithemepark.bandicore.util.npc.NPC
 import net.kyori.adventure.text.format.NamedTextColor
-import net.kyori.adventure.text.format.TextColor
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
-import org.bukkit.Color
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.scoreboard.Scoreboard
 import org.bukkit.scoreboard.Team
-import java.text.SimpleDateFormat
-import java.util.*
 
 class BandiScoreboard {
     val mainScoreboard: Scoreboard = Bukkit.getScoreboardManager().newScoreboard
+
+    private fun setupScoreboardTeams() {
+        // Register teams for all ranks
+        for (rank in BandiCore.instance.server.rankManager.loadedRanks) {
+            val team = mainScoreboard.registerNewTeam(rank.scoreboardName)
+            team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER)
+            team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER)
+        }
+
+        // Register team for NPCs
+        val npcTeam = mainScoreboard.registerNewTeam("npc")
+        npcTeam.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER)
+
+        // Register team for each color (used for hover)
+        for(color in NamedTextColor.NAMES.keys()) {
+            val team = mainScoreboard.registerNewTeam(color)
+            team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER)
+            team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER)
+            team.color(NamedTextColor.NAMES.value(color))
+        }
+    }
+
+    private fun clearScoreboardTeams() {
+        val teams = mainScoreboard.teams.toMutableList()
+        for(team in teams) {
+            team.unregister()
+        }
+    }
 
     /**
      * Updates the main scoreboard, and applies it to everyone
      */
     fun updateScoreboard() {
-        // Register teams if they have not already been registered
-        if(mainScoreboard.teams.isEmpty()) {
-            // Register teams for all ranks
-            for (rank in BandiCore.instance.server.rankManager.loadedRanks) {
-                val team = mainScoreboard.registerNewTeam(rank.scoreboardName)
-                team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER)
-                team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER)
-            }
+        // Re-register all teams, so that they are cleared
+        clearScoreboardTeams()
+        setupScoreboardTeams()
 
-            // Register team for NPCs
-            val npcTeam = mainScoreboard.registerNewTeam("npc")
-            npcTeam.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER)
-
-            // Register team for each color (used for hover)
-            for(color in ChatColor.values()) {
-                val team = mainScoreboard.registerNewTeam(color.name)
-                team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER)
-                team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER)
-                team.color(paperColors[color])
-            }
-        }
-
+        // Add players of each rank to their corresponding team
         for (rank in BandiCore.instance.server.rankManager.loadedRanks) {
             val team = mainScoreboard.getTeam(rank.scoreboardName)!!
 
             // Add all players with the rank to the team
             for (player in BandiCore.instance.server.rankManager.loadedPlayerRanks.keys.filter { rank == BandiCore.instance.server.rankManager.loadedPlayerRanks[it] }) {
-                if(!team.hasEntry(player.name)) team.addEntry(player.name)
+                team.addEntry(player.name)
             }
         }
 
         // Update team entries for each color
-        for(color in ChatColor.values()) {
+        for(color in ChatColor.entries) {
             val team = mainScoreboard.getTeam(color.name)!!
 
             // Add all entities with the color to the team
             for ((key, value) in selectedColors.entries.toSet()) {
                 if (value == color) {
-                    if(!team.hasEntry(key)) team.addEntry(key)
+                    team.addEntry(key)
                 }
             }
         }
 
-        // Clear NPC team
-        val npcTeam = mainScoreboard.getTeam("npc")!!
-
         // Add NPCs to team
+        val npcTeam = mainScoreboard.getTeam("npc")!!
         for(npc in NPC.active) {
-            if(!npcTeam.hasEntry(npc.name)) npcTeam.addEntry(npc.name)
+            npcTeam.addEntry(npc.name)
         }
 
         // Tablist updates
+        updatePlayerTabListNames()
+    }
+
+    private fun updatePlayerTabListNames() {
         for(player in Bukkit.getOnlinePlayers()) {
             // Update name
             val rank = BandiCore.instance.server.rankManager.loadedPlayerRanks[player]
@@ -109,26 +118,6 @@ class BandiScoreboard {
     fun setGlowColor(entityUUID: String, color: ChatColor) {
         selectedColors[entityUUID] = color
     }
-
-    val paperColors = hashMapOf<ChatColor, NamedTextColor>(
-        ChatColor.RED to NamedTextColor.RED,
-        ChatColor.DARK_RED to NamedTextColor.DARK_RED,
-        ChatColor.BLUE to NamedTextColor.BLUE,
-        ChatColor.DARK_BLUE to NamedTextColor.DARK_BLUE,
-        ChatColor.GREEN to NamedTextColor.GREEN,
-        ChatColor.DARK_GREEN to NamedTextColor.DARK_GREEN,
-        ChatColor.YELLOW to NamedTextColor.YELLOW,
-        ChatColor.GOLD to NamedTextColor.GOLD,
-        ChatColor.AQUA to NamedTextColor.AQUA,
-        ChatColor.DARK_AQUA to NamedTextColor.DARK_AQUA,
-        ChatColor.LIGHT_PURPLE to NamedTextColor.LIGHT_PURPLE,
-        ChatColor.DARK_PURPLE to NamedTextColor.DARK_PURPLE,
-        ChatColor.GOLD to NamedTextColor.GOLD,
-        ChatColor.GRAY to NamedTextColor.GRAY,
-        ChatColor.DARK_GRAY to NamedTextColor.DARK_GRAY,
-        ChatColor.BLACK to NamedTextColor.BLACK,
-        ChatColor.WHITE to NamedTextColor.WHITE
-    )
 
     companion object {
         fun Player.updatePlayerList() {
