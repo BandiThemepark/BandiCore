aabf8a581a82d9eaebfe2df4d91880cb3fcb3a49
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2025-01-01 22:34:47 +0100

    Add LeaderboardEntryText to easily render entry text

---
+++ src/main/java/net/bandithemepark/bandicore/server/leaderboards/display/LeaderboardEntryText.kt
@@ -0,0 +1,52 @@
+package net.bandithemepark.bandicore.server.leaderboards.display
+
+import net.bandithemepark.bandicore.server.leaderboards.LeaderboardEntry
+import org.bukkit.Color
+import org.bukkit.Location
+
+class LeaderboardEntryText(var entry: LeaderboardEntry, baseLocation: Location, val index: Int, val regionId: String? = null) {
+    companion object {
+        const val TEXT_SCALE = 1.0
+        const val DISTANCE_BETWEEN_ENTRIES = 0.25
+        const val DISTANCE_BETWEEN_NAME_AND_SCORE = 0.1
+        const val ENTRIES_START_HEIGHT = 1.7
+    }
+
+    private var nameText = LeaderboardText(
+        entry.name,
+        baseLocation.clone().add(0.0, ENTRIES_START_HEIGHT - (index * DISTANCE_BETWEEN_ENTRIES), 0.0),
+        Color.WHITE, TEXT_SCALE,
+        regionId
+    )
+
+    private var scoreText  = LeaderboardText(
+        entry.score,
+        baseLocation.clone().add(0.0, ENTRIES_START_HEIGHT - (index * DISTANCE_BETWEEN_ENTRIES) - DISTANCE_BETWEEN_NAME_AND_SCORE, 0.0),
+        Color.WHITE, TEXT_SCALE,
+        regionId
+    )
+
+    var spawned = false
+    fun spawn() {
+        if(spawned) return
+
+        nameText.spawn()
+        scoreText.spawn()
+
+        spawned = true
+    }
+
+    fun deSpawn() {
+        if (!spawned) return
+
+        nameText.deSpawn()
+        scoreText.deSpawn()
+
+        spawned = false
+    }
+
+    fun update() {
+        nameText.text = entry.name
+        scoreText.text = entry.score
+    }
+}
--- src/main/java/net/bandithemepark/bandicore/server/leaderboards/display/LeaderboardText.kt
+++ src/main/java/net/bandithemepark/bandicore/server/leaderboards/display/LeaderboardText.kt
@@ -8,8 +8,21 @@ import org.bukkit.Location
 import org.bukkit.entity.Display
 import org.bukkit.entity.TextDisplay
 
-class LeaderboardText(val text: String, val location: Location, val color: Color, val scale: Double, val regionId: String? = null) {
+/**
+ * An instance of a text display in the world, with some default configuration for display on leaderboards
+ * The text will update itself when the text property is set.
+ */
+class LeaderboardText(text: String, val location: Location, val color: Color, val scale: Double, val regionId: String? = null) {
     var textDisplay: PacketTextDisplay? = null
+
+    var text: String = text
+        set(value) {
+            field = value
+
+            if(!spawned) return
+            textDisplay?.setText(Util.color("<${color.toHexString()}>$value"))
+            textDisplay?.updateMetadata()
+        }
 
     var spawned = false
     fun spawn() {
