174611ab8fcfcf809b2ff6b2aa1a1484ad5324c9
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2025-01-01 22:28:35 +0100

    Setup LeaderboardText utility for re-usable text displays

---
+++ src/main/java/net/bandithemepark/bandicore/server/leaderboards/display/LeaderboardText.kt
@@ -0,0 +1,37 @@
+package net.bandithemepark.bandicore.server.leaderboards.display
+
+import net.bandithemepark.bandicore.util.Util
+import net.bandithemepark.bandicore.util.Util.toHexString
+import net.bandithemepark.bandicore.util.entity.display.PacketTextDisplay
+import org.bukkit.Color
+import org.bukkit.Location
+import org.bukkit.entity.Display
+import org.bukkit.entity.TextDisplay
+
+class LeaderboardText(val text: String, val location: Location, val color: Color, val scale: Double, val regionId: String? = null) {
+    var textDisplay: PacketTextDisplay? = null
+
+    var spawned = false
+    fun spawn() {
+        if(spawned) return
+
+        textDisplay = PacketTextDisplay()
+        textDisplay!!.spawn(location, regionId)
+        textDisplay!!.setText(Util.color("<${color.toHexString()}>$text"))
+        textDisplay!!.setBillboard(Display.Billboard.FIXED)
+        textDisplay!!.setAlignment(TextDisplay.TextAlignment.CENTER)
+        textDisplay!!.setScale(scale)
+        textDisplay!!.updateMetadata()
+
+        spawned = true
+    }
+
+    fun deSpawn() {
+        if(!spawned) return
+
+        textDisplay?.deSpawn()
+        textDisplay = null
+
+        spawned = false
+    }
+}
