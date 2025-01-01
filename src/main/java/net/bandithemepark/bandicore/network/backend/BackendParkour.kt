c438f5cdebabca68de997c70f3240bb097692397
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2025-01-02 00:44:28 +0100

    Add method to get top ten of a parkour

--- src/main/java/net/bandithemepark/bandicore/network/backend/BackendParkour.kt
+++ src/main/java/net/bandithemepark/bandicore/network/backend/BackendParkour.kt
@@ -4,7 +4,6 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import net.bandithemepark.bandicore.BandiCore
-import net.bandithemepark.bandicore.util.Util
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -43,6 +42,30 @@ object BackendParkour {
                 } else {
                     BandiCore.instance.logger.severe("An attempt was made at saving the parkour time of $playerUUID, but the attempt failed. The following message was given: ${responseJson.get("message")}")
                     callback.invoke(false)
+                }
+            }
+        })
+    }
+
+    fun getTopTenOfParkour(parkourId: String, callback: (data: JsonArray) -> Unit) {
+        val client = BandiCore.instance.okHttpClient
+        val request = Request.Builder()
+            .url("https://api.bandithemepark.net/timeseries/parkour/top/$parkourId")
+            .method("GET", null)
+            .header("Authorization", BandiCore.instance.server.apiKey)
+            .build()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseJson = JsonParser().parse(response.body!!.string()).asJsonObject
+                if (responseJson.has("data") && !responseJson.get("data").isJsonNull) {
+                    callback.invoke(responseJson.getAsJsonArray("data"))
+                } else {
+                    BandiCore.instance.logger.severe("An attempt was made at getting the top of the parkour $parkourId, but the attempt failed. The following message was given: ${responseJson.get("message")}")
                 }
             }
         })
