ce578e5f6b9ace99e02c939f16ef7c6ae7d124bd
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2025-01-20 22:22:43 +0100

    Add network class to connect Discord account

---
+++ src/main/java/net/bandithemepark/bandicore/network/backend/discord/BackendDiscordConnector.kt
@@ -0,0 +1,59 @@
+package net.bandithemepark.bandicore.network.backend.discord
+
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import net.bandithemepark.bandicore.BandiCore
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.IOException
+import java.util.UUID
+
+object BackendDiscordConnector {
+    fun connect(playerUUID: UUID, token: String, callback: () -> Unit) {
+        val client = BandiCore.instance.okHttpClient
+        val mediaType = "application/json".toMediaTypeOrNull()
+
+        val json = JsonObject()
+        json.addProperty("playerUuid", playerUUID.toString())
+        json.addProperty("connectionToken", token)
+
+        val request = Request.Builder()
+            .url("https://api.bandithemepark.net/discord/connect")
+            .method("POST", json.toString().toRequestBody(mediaType))
+            .header("Authorization", BandiCore.instance.server.apiKey)
+            .build()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseJson = JsonParser().parse(response.body!!.string()).asJsonObject
+
+                val message = responseJson.get("message").asString
+                when(message.lowercase()) {
+                    "not found" -> {
+                        throw PlayerWithUUIDNotFoundException()
+                    }
+                    "already exists" -> {
+                        throw DiscordAlreadyConnectedException()
+                    }
+                    "invalid" -> {
+                        throw DiscordTokenExpiredException()
+                    }
+                    "success" -> {
+                        callback.invoke()
+                    }
+                    else -> {
+                        throw DiscordConnectFailedException()
+                    }
+                }
+            }
+        })
+    }
+}
