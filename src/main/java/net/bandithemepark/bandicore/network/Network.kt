e0bfe45cad1c61a42fbc68b02e197fa75143ddbc
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2025-01-27 21:21:56 +0100

    Make network call util suspended

--- src/main/java/net/bandithemepark/bandicore/network/Network.kt
+++ src/main/java/net/bandithemepark/bandicore/network/Network.kt
@@ -1,6 +1,7 @@
 package net.bandithemepark.bandicore.network
 
 import com.google.gson.JsonObject
+import kotlinx.coroutines.suspendCancellableCoroutine
 import net.bandithemepark.bandicore.BandiCore
 import okhttp3.Call
 import okhttp3.Callback
@@ -9,68 +10,77 @@ import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 object Network {
     /**
      * Perform a GET call on the API
      * @param url The URL to call, such as `/warps`
-     * @param callback The callback to run when the request is complete
      */
-    fun get(url: String, callback: (Response) -> Unit) {
-        fetch(Method.GET, url, null, callback)
+    suspend fun get(url: String): Response {
+        return fetch(Method.GET, url, null)
     }
 
     /**
      * Perform a GET call on the API
      * @param url The URL to call, such as `/warps`
      * @param body The body to send with the request
-     * @param callback The callback to run when the request is complete
      */
-    fun post(url: String, body: JsonObject, callback: (Response) -> Unit) {
-        fetch(Method.POST, url, body, callback)
+    suspend fun post(url: String, body: JsonObject): Response {
+        return fetch(Method.POST, url, body)
     }
 
     /**
      * Perform a GET call on the API
      * @param url The URL to call, such as `/warps`
      * @param body The body to send with the request
-     * @param callback The callback to run when the request is complete
      */
-    fun put(url: String, body: JsonObject, callback: (Response) -> Unit) {
-        fetch(Method.PUT, url, body, callback)
+    suspend fun put(url: String, body: JsonObject): Response {
+        return fetch(Method.PUT, url, body)
     }
 
     /**
      * Perform a GET call on the API
      * @param url The URL to call, such as `/warps`
-     * @param body The body to send with the request
-     * @param callback The callback to run when the request is complete
      */
-    fun delete(url: String, callback: (Response) -> Unit) {
-        fetch(Method.DELETE, url, null, callback)
+    suspend fun delete(url: String): Response {
+        return fetch(Method.DELETE, url, null)
     }
 
     val client = BandiCore.instance.okHttpClient
     val mediaType = "application/json".toMediaTypeOrNull()
     val apiKey = BandiCore.instance.server.apiKey
 
-    private fun fetch(method: Method, url: String, body: JsonObject?, callback: (Response) -> Unit) {
-
+    private suspend fun fetch(method: Method, url: String, body: JsonObject?): Response {
+        val requestBody = body?.toString()?.toRequestBody(mediaType)
         val request = Request.Builder()
             .url("https://api.bandithemepark.net$url")
-            .method(method.name, body.toString().toRequestBody(mediaType))
+            .method(method.name, requestBody)
             .header("Authorization", apiKey)
             .build()
 
-        client.newCall(request).enqueue(object: Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
+        return suspendCancellableCoroutine { continuation ->
+            val call = client.newCall(request)
+
+            continuation.invokeOnCancellation {
+                call.cancel()
             }
 
-            override fun onResponse(call: Call, response: Response) {
-                callback.invoke(response)
-            }
-        })
+            call.enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    if (continuation.isActive) {
+                        continuation.resumeWithException(e)
+                    }
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (continuation.isActive) {
+                        continuation.resume(response)
+                    }
+                }
+            })
+        }
     }
 
     enum class Method {
