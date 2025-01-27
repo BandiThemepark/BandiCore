ed520096665122a6b61605a2336b0ac2b004e6fb
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2025-01-27 21:22:50 +0100

    Make discord API call use coroutines

--- src/main/java/net/bandithemepark/bandicore/network/backend/discord/BackendDiscordConnector.kt
+++ src/main/java/net/bandithemepark/bandicore/network/backend/discord/BackendDiscordConnector.kt
@@ -2,6 +2,8 @@ package net.bandithemepark.bandicore.network.backend.discord
 
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
+import kotlinx.coroutines.launch
+import net.bandithemepark.bandicore.BandiCore.Companion.pluginScope
 import net.bandithemepark.bandicore.network.Network
 import java.util.UUID
 
@@ -16,13 +18,15 @@ object BackendDiscordConnector {
         json.addProperty("playerUuid", playerUUID.toString())
         json.addProperty("connectionToken", token)
 
-        Network.post(
-            "/discord/connect",
-            json,
-        ) { response ->
-            val responseJson = JsonParser().parse(response.body!!.string()).asJsonObject
+        pluginScope.launch {
+            val response = Network.post(
+                "/discord/connect",
+                json,
+            )
 
+            val responseJson = JsonParser().parse(response.body!!.string()).asJsonObject
             val message = responseJson.get("message").asString
+
             when(message.lowercase()) {
                 "not found" -> onError(PlayerWithUUIDNotFoundException())
                 "already exists" -> onError(DiscordAlreadyConnectedException())
